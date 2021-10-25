acd512aad494eb45e2479a61e5fb1145f435e995
leo848 <leonardo.blume@gymnasium-essen-werden.de>
2021-10-25 12:42:47 +0200

    distribute mines not done yet

--- src/io/github/leo848/MinesweeperCanvas.java
+++ src/io/github/leo848/MinesweeperCanvas.java
@@ -1,23 +1,29 @@
 package io.github.leo848;
 
-import javax.swing.JPanel;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
+import javax.swing.*;
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static java.util.Arrays.asList;
+import static java.util.Arrays.stream;
 
 public class MinesweeperCanvas extends JPanel {
+	final Vector scale = new Vector();
 	Vector size = new Vector(500, 500);
-	private GameLoop gameLoop;
-	
 	List<List<Integer>> grid;
+	private GameLoop gameLoop;
+	private Random random;
 	
 	public MinesweeperCanvas(GameLoop gameLoop) {
 		this.gameLoop = gameLoop;
 		
 		setPreferredSize(new Dimension((int) size.x, (int) size.y));
+		scale.set(size.x / 10, size.y / 10);
+		
 		fillGrid(10, 10);
 	}
 	
@@ -28,14 +34,36 @@ public class MinesweeperCanvas extends JPanel {
 		g2D.setColor(new Color(0x0));
 		g2D.fillRect(0, 0, 500, 500);
 		
+		g2D.setColor(new Color(0xffffff));
+		for (int x = 0; x < grid.size(); x++) {
+			for (int y = 0; y < grid.get(x).size(); y++) {
+				int number = grid.get(x).get(y);
+				if (number == -1) {
+					g2D.setColor(new Color(0xff0000));
+					g2D.fillRect((int) (x * scale.x + 5), (int) (y * scale.y + 5), (int) (scale.x - 10), (int) (scale.y - 10));
+				}
+			}
+		}
+	}
+	
+	public void distributeMines(int amount) {
+		List<Integer> possibilities = IntStream
+				.range(0, grid.size() * grid.get(0).size())
+				.boxed()
+				.collect(Collectors.toList());
 		
+		for (int i = 0; i < amount; i++) {
+			int randomNumber = possibilities.get;
+		}
 	}
 	
 	public void fillGrid(int width, int height) {
+		grid = new ArrayList<>(width);
+		
 		for (int x = 0; x < width; x++) {
-			grid.set(x, new ArrayList<>(height));
+			grid.add(new ArrayList<>(height));
 			for (int y = 0; y < height; y++) {
-				grid.get(x).set(y, 0);
+				grid.get(x).add(0);
 			}
 		}
 	}
